298ec49b7c237423dd002da81a8e84bdc075ed6d
Elliscope Fang <mingzhef@usc.edu>
2015-01-13 18:30:05 +0800

    test to fix the close problem

--- img.js
+++ img.js
@@ -1,7 +1,7 @@
 function image_open(mySource) {
     document.getElementById('viewer').style.visibility = 'visible';
     document.getElementById('viewer').style.opacity = '1';
-    document.getElementById('viewer').style.filter = 'alpha(opacity=100)';
+    document.getElementById('viewer').style.filter = 'alpha(opacity=0)';
     myURL = mySource.style.backgroundImage.slice(4, -1).replace("\"", "").replace("\"", "");
     document.getElementById('viewer_image').src = myURL;
     document.getElementById('viewer_link').href = myURL;
